0f0b12b1450446e1ade8a3045a60b3474ab83646
mil <1363944642@qq.com>
2020-09-27 15:35:55 +0800

    异步语法

---
+++ src/playground.ts
@@ -0,0 +1,42 @@
+// const nature = callback => {
+//   const data = '🐿';
+//   callback(data);
+// };
+
+// nature(data => {
+//   console.log(data);
+// });
+
+// var ji;
+// const nature = () => {
+//   console.log('🥚');
+//   setTimeout(() => {
+//     ji = '🐥';
+//   }, 2000);
+// };
+
+// nature();
+// console.log(ji);
+// console.log('🐣');
+
+// const nature = () => {
+//   console.log('🥚');
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve('🐥');
+//     }, 2000);
+//   });
+// };
+
+// nature().then(data => {
+//   console.log(data);
+// });
+
+// const demo = async () => {
+//   const data = await nature();
+//   console.log(data);
+// };
+
+// demo();
+
+// console.log('🐣');
--- src/post/post.router.ts
+++ src/post/post.router.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import * as postController from './post.controller';
 import { requestUrl } from '../app/app.middleware';
+import { read } from 'fs';
 
 const router = express.Router();
 
@@ -8,6 +9,10 @@ const router = express.Router();
  * 内容列表
  */
 router.get('/posts', requestUrl, postController.index);
+
+router.post('/posts', function(req, res) {
+  res.send(req.body);
+});
 
 /**
  * 导出路由
