924e61845e434238810da43389a9c4bf056d245b
mil <1363944642@qq.com>
2020-10-03 23:13:30 +0800

    添加file.router注释

--- src/file/file.router.ts
+++ src/file/file.router.ts
@@ -9,6 +9,7 @@ const router = express.Router();
  * 上传文件
  * authGuard:通过公钥验证客户端令牌,并得到令牌中的用户信息存放在请求处理参数中
  * fileInterceptor:Multer文件拦截器
+ * fileProcessor:在请求里添加照片详细数据(如图片大小 相机型号等)request.fileMetaData,生成不同尺寸的图像文件储存到uploads/resized文件夹中
  * fileController.store:获取文件信息和关联用户id与关联内容id并储存到数据库中
  */
 router.post(
@@ -21,7 +22,9 @@ router.post(
 
 /**
  * 文件服务
- * fileController.serve:通过从地址参数里得到文件ID在数据库中查找到对应数据,通过response.sendFile返回给客户端
+ * fileController.serve:通过从地址参数里得到文件ID在数据库中查找到对应数据,
+ * 通过 size 查询参数,获得响应大小图像尺寸的文件名与路径
+ * 通过response.sendFile把图片返回给客户端
  */
 router.get('/files/:fileId/serve', fileController.serve);
 
