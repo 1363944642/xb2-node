41ce8efe4c7689ae7da44b4f09cf2f503bf65dab
mil <1363944642@qq.com>
2020-10-04 23:08:22 +0800

    修改评论接口与删除评论接口

--- src/comment/comment.router.ts
+++ src/comment/comment.router.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { authGuard } from '../auth/auth.middleware';
+import { accessControl, authGuard } from '../auth/auth.middleware';
 import * as commentController from './comment.controller';
 
 const router = express.Router();
@@ -15,6 +15,32 @@ router.post('/comments', authGuard, commentController.store);
  * 回复评论
  */
 router.post('/comments/:commentId/reply', authGuard, commentController.reply);
+
+/**
+ * 修改评论
+ * insertId:通过公钥验证客户端令牌,并得到令牌中的用户信息,存放在请求处理参数中.
+ * accessControl({ possession: true }):验证用户是否拥有要修改的这个评论资源(就是这个评论是否是当前用户发表的)
+ * commentController.update:准备数据,在数据库修改评论数据
+ */
+router.patch(
+  '/comments/:commentId',
+  authGuard,
+  accessControl({ possession: true }),
+  commentController.update,
+);
+
+/**
+ * 删除评论
+ * insertId:通过公钥验证客户端令牌,并得到令牌中的用户信息,存放在请求处理参数中.
+ * accessControl({ possession: true }):验证用户是否拥有要删除的这个评论资源(就是这个评论是否是当前用户发表的)
+ * commentController.update:准备数据,在数据库修改评论数据
+ */
+router.delete(
+  '/comments/:commentId',
+  authGuard,
+  accessControl({ possession: true }),
+  commentController.destroy,
+);
 
 /**
  * 导出路由
