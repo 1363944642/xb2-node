27cd46bd71c0776902a63a73917622b960664fd2
mil <1363944642@qq.com>
2020-10-07 17:06:47 +0800

    定义文件过滤器_定义上传头像接口_调整头像尺寸

---
+++ src/avarar/avatar.service.ts
@@ -0,0 +1,2 @@
+import { fromPairs } from 'lodash';
+import { connection } from '../app/database/mysql';
--- src/file/file.middleware.ts
+++ src/file/file.middleware.ts
@@ -1,13 +1,37 @@
 import { Request, Response, NextFunction } from 'express';
-import multer from 'multer';
+import multer, { FileFilterCallback } from 'multer';
 import Jimp from 'jimp';
 import { imageResizer } from './file.servive';
 
+/**
+ * 文件过滤器
+ */
+export const fileFilter = (fileTypes: Array<string>) => {
+  return (
+    request: Request,
+    file: Express.Multer.File,
+    callback: FileFilterCallback,
+  ) => {
+    //测试文件类型
+    const allowed = fileTypes.some(type => type === file.mimetype);
+
+    if (allowed) {
+      // 允许上传
+      callback(null, true);
+    } else {
+      // 拒绝上传
+      callback(new Error('FILE_TYPE_NOT_ACCEPT'));
+    }
+  };
+};
+
+const fileUploadFilter = fileFilter(['image/png', 'image/jpg', 'image/jpeg']);
 /**
  * 创建一个 Multer
  */
 const fileUpload = multer({
   dest: 'uploads/', //上传文件存储的位置
+  fileFilter: fileUploadFilter, //文件过滤器
 });
 
 /**
